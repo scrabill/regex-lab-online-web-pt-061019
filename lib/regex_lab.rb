cf2c1889d36b42168581187320fc215f15a16df5
Shannon V. Crabill <shannon.crabill@gmail.com>
2019-07-17 17:57:47 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,4 +1,7 @@
 def starts_with_a_vowel?(word)
+  # word.match(\s[aeiou])
+  # word.match(/\b[aeiou]/)
+  word.match(/\s+[a|e|i|o|u|A|E|I|O|U]/)
 
 end
 
@@ -7,6 +10,7 @@ def words_starting_with_un_and_ending_with_ing(text)
 end
 
 def words_five_letters_long(text)
+  text.scan(/\w{5}/)
 
 end
 
@@ -15,5 +19,10 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
+  if phone.scan(/(\d+)-(\d+)-(\d+)/) == true
+    true
+  else
+    false
+  end
 
 end
